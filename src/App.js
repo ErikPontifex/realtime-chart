0e1b9f3aea0ae390469f6cf033007a9c5e4983d7
Erik Pontifex <erikpon@gmail.com>
2018-11-26 21:10:23 -0800

    Added chart and live data updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { AreaChart } from 'react-easy-chart';
 import './App.css';
 
+const UPDATE_SPEED = 1000; // update interval in milliseconds
+
+const updateData = (data) => {
+  return [
+    ...data
+        .slice(data.length - 51)
+        .map(point => ({ x: point.x - UPDATE_SPEED, y: point.y })),
+
+      {x: 0, y: Math.random() * 25}
+  ];
+};
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        data: updateData(this.state.data),
+      });
+    }, UPDATE_SPEED);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AreaChart
+        axes
+        axisLabels={{x: 'time', y: 'Mbps'}}
+        grid
+        verticalGrid
+        margin={{top: 30, right: 30, bottom: 30, left: 30}}
+        width={1000}
+        height={500}
+        interpolate={'cardinal'}
+        data={[this.state.data]}
+        xTicks={10}
+        yDomainRange={[0, 28]}
+        yAxisOrientRight
+      />
     );
   }
 }
